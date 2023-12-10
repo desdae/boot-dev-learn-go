2d58a124e3f4c6f7a1a8aabd1f866f17d47a812a
desdae <desdae@gmail.com>
2023-12-10 19:59:50 +0200

    2.1B Short Variable Declaration

--- ch2/main.go
+++ ch2/main.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// initialize variables here
-	var smsSendingLimit int
-	var costPerSMS float64
-	var hasPermission bool
-	var username string
+	// declare here
 
-	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	fmt.Println(congrats)
 }
